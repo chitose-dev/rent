fd22033e914c61ea8e339edba15082c15f08e5a2
Kamiogi <kamiogi@chitosedev.com>
2026-02-22 18:00:28 +0900

    debug: ログイン認証のデバッグログを追加

--- admin/common.js
+++ admin/common.js
@@ -49,7 +49,10 @@ const AdminAuth = {
   
   // 認証チェック（ページ読み込み時）
   checkAuth() {
+    const token = this.getToken();
+    console.log('[AdminAuth.checkAuth] adminToken:', token ? token.substring(0, 20) + '...' : 'null');
     if (!this.isLoggedIn()) {
+      console.log('[AdminAuth.checkAuth] Not logged in, redirecting to login.html');
       window.location.href = 'login.html';
       return false;
     }
@@ -179,8 +182,12 @@ function adminLogout() {
 }
 
 // ページ読み込み時にログインチェック
+console.log('[common.js] pathname:', window.location.pathname);
 if (!window.location.pathname.endsWith('login.html')) {
+  console.log('[common.js] Not login.html, checking auth...');
   checkAdminAuth();
+} else {
+  console.log('[common.js] On login.html, skipping auth check');
 }
 
 // ハンバーガーメニュー
